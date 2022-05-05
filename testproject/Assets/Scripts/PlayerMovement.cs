2254dd27dffb741619451319747733babb2b28b4
Roland Hemmelmayr MacBookPro <inspiredorange@me.com>
2022-05-05 18:37:00 +0200

    player movement with gyro sensor and arrow keys

--- testproject/Assets/Scripts/PlayerMovement.cs
+++ testproject/Assets/Scripts/PlayerMovement.cs
@@ -31,8 +31,31 @@ public class PlayerMovement : NetworkBehaviour
     {
         if (IsLocalPlayer)
         {
-            transform.position += Input.GetAxis("Vertical") * m_Speed * Time.fixedDeltaTime * transform.forward;
-            transform.rotation = Quaternion.Euler(0, Input.GetAxis("Horizontal") * 90 * m_RotSpeed * Time.fixedDeltaTime, 0) * transform.rotation;
+            float speed = 0f;
+            float rot = 0f;
+
+            if (Application.isEditor)
+            {
+                speed = Input.GetAxis("Vertical");
+                rot = Input.GetAxis("Horizontal");
+            }
+            else
+            {
+                if (!Input.gyro.enabled)
+                {
+                    Input.gyro.enabled = true;
+                    Debug.LogWarning("*** GYRO SENSOR ENABLED ***");
+                }
+                if (Input.gyro != null && Input.gyro.enabled)
+                {
+                    const float damper = 33f;
+                    speed = Input.gyro.rotationRate.x * damper;
+                    rot =  Input.gyro.rotationRate.y ;
+                }
+            }
+
+            transform.position += speed * 0.1f * m_Speed * Time.fixedDeltaTime * transform.forward;
+            transform.rotation = Quaternion.Euler(0, rot * 90f * m_RotSpeed * Time.fixedDeltaTime, 0) * transform.rotation;
         }
     }
 }
